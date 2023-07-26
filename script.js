e462a4e44cec04fc0b44e6235ebaa5a2d48768a1
Marina Del Conte <marinadelconte@yahoo.com>
2023-07-26 14:52:21 -0400

    corrected the code so that the button is working when you open the browser

--- script.js
+++ script.js
@@ -2,48 +2,47 @@ let generateBtn = document.querySelector("#generate");
 
 let lowerAlphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 let upperAlphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-let numbers = [0,1,2,3,4,5,6,7,8,9];
+let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 let specialCharacters = [];
 
 let builtArray = [];
 let password = "";
 
-generatePassword();
+// generatePassword();
 
 function generatePassword() {
-if (confirm("Do you want to include lowercase letters?"))
-{
-  builtArray = [...builtArray, ...lowerAlphabet];
-} 
+  if (confirm("Do you want to include lowercase letters?")) {
+    console.log("before updating build array", builtArray)
+    builtArray = [...builtArray, ...lowerAlphabet];
+    console.log("after updating build array", builtArray)
+  }
 
-if (confirm("Do you want to include uppercase letters?"))
-{
-  builtArray = [...builtArray, ...lowerAlphabet, ...upperAlphabet];
-} 
+  if (confirm("Do you want to include uppercase letters?")) {
+    builtArray = [...builtArray, ...upperAlphabet];
+  }
 
-if (confirm("Do you want to include numbers?"))
-{
-  builtArray = [...builtArray, ...lowerAlphabet, ...upperAlphabet];
-} 
+  if (confirm("Do you want to include numbers?")) {
+    builtArray = [...builtArray, ...numbers];
+  }
 
-if (confirm("Do you want to include special characters?"))
-{
-  builtArray = [...builtArray, ...lowerAlphabet, ...upperAlphabet];
-} 
-
-let passwordLength = prompt("How long do you want the password?")
-for (let i = 0; i < passwordLength; i++) {
-  password += "x";
-}
-}
+  if (confirm("Do you want to include special characters?")) {
+    builtArray = [...builtArray, ...specialCharacters];
+  }
   
+  let passwordLength = prompt("How long do you want the password?")
+  for (let i = 0; i < passwordLength; i++) {
+    password += "x";
+
+  }
+}
 
 
-return password;
+
 
 
 
 function writePassword() {
+  console.log("buttonPressed")
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
